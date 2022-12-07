e50befae8e503b2a5328d81ec74450f9864263e7
Liam <ldillondavidson@smith.edu>
2022-12-07 11:23:30 -0500

    Revised improvements

--- README.md
+++ README.md
@@ -1,22 +1,45 @@
 # MedTrak-Farkle
 
-I did not have time to implement the features I originally planned, due to the fact that
-relative unfamiliarity with Javascript/HTML made me need to spend a good mount of time checking syntax,
-functions etc, and figuring out what would be feasible for me to do in this amount of time.
+Original submission notes:I did not have time to implement the features I originally planned, due to the fact that relative unfamiliarity with Javascript/HTML made me need to spend a good mount of time checking syntax,functions etc, and figuring out what would be feasible for me to do in this amount of time.
 
 ## What I started but abandoned due to time awareness, less critical:
 
-- displaying the score where the graphics imply it should be 
-- have the Farkle message (if a dice roll results in score of 0) display on screen instead of to console,
-	same for '5 turns' message
+- ~~displaying the score where the graphics imply it should be~~
+- have the Farkle message (if a dice roll results in score of 0) display on screen instead of to console, same for '5 turns' message
 
 ## The plan for what I would keep working on/implement if given the opportunity, most critical:
 
-- modified, single player Farkle game of chance, where the challenge is to see if you can beat 5,000 points
-	in 5 turns or less (to be adjusted as necessary)
-- currently the program autocalculates your max score for each roll of the dice with the player having to
-	click anything, I would finish implementing the functionality that requires the player to click before
-	a particular die is including in the score for that turn
-- ultimately make a more complicated multiplayer game that also accounts for the choice a player has 
-	in terms of either banking their points for a particular roll and passing, or going again, which I laid the groundwork
-	for and mapped out in a pseudocode diagram
+- ~~modified, single player Farkle game of chance~~, where the challenge is to see if you can beat 5,000 points in 5 turns or less (to be adjusted as necessary)
+- ~~currently the program autocalculates your max score for each roll of the dice with the player having to click anything, I would finish implementing the functionality that requires the player to click before a particular die is including in the score for that turn~~
+- ultimately make a more complicated multiplayer game that also accounts for the choice a player has in terms of either banking their points for a particular roll and passing, or going again, which I laid the groundwork for and mapped out in a pseudocode diagram
+
+---
+
+## Improvements made since the original submission
+
+### Game features functionality and .js changes
+
+- Dice only get counted towards score if they are clicked
+- Score for each dice roll counted, as well as final cumulative score over all 5 rounds
+- TODO: Make it so score is not cumulative over turn, sums up separate roll scores separately
+
+### GUI
+
+- Background gradient and bank button color/hover now both colorblind friendly
+  (palettes taken from https://davidmathlogic.com/colorblind/)
+- Changed fonts styles and sizes
+- Dice go transparent when clicked, reset at the end of each turn
+- Hover transparency on buttons
+- Buttons get double outline
+- Score displays to correct place, also displays turn count in matching style
+- Separate button to calculate score for each roll and to bank total points for that turn
+
+## Next steps ideas, most critical
+
+- When you get Farkle on a dice roll, get 0 points for that turn and turn is over automatically.
+- Instead of implementing human multiplayer, a good intermediary step idea was to play against the computer: each turn, will roll the dice for the "computer player" also and calculate max score for the computer. The goal would be to beat the computer in 5 turns, and although less robust than multiple human players, still implements a more interactive element to make it a true competitive game of chance.
+- If I started from beginning again, would try to implement a MVC architecture to be more organized and scalable and implement classes more in general.
+
+## Next steps ideas, less critical
+
+- Display rules/guidelines on screen to make usage clear for human user (especially for current state of play: i.e., must always click calculate roll score before banking points, roll scores are cumulative over turn, objective is to beat xxxx points by end of 5 turns).
--- farkle.js
+++ farkle.js
@@ -1,118 +1,129 @@
-var diceArr = [];
-var turnCount = 0;
+/* Farkle game for one player.
 
+Developed by Liam Knight for CareSense.*/
 
-/* Changed to diceArr[i].id = "die" + (i + 1);
-added parentheses, seemed to be an error in setting
-the ids of the dice*/
+// Initialize variables
+let diceArr = [];
+let turnCount = 0;
+let ultimateScore = 0;
+let rollScoresForTurnArr = [];
+let turnScoresForTotalArr = [];
+
+/* Changed to diceArr[i].id = "die" + (i + 1); 
+added parentheses, seemed to be an error in setting the ids of the dice.*/
 function initializeDice(){
 	for(i = 0; i < 6; i++){
 		diceArr[i] = {};
 		diceArr[i].id = "die" + (i + 1);
 		diceArr[i].value = i + 1;
 		diceArr[i].clicked = 0;
+		//diceArr[i].counted = false;
 	}
-	
 }
 
-
-/*Rolling dice values*/
+/*Rolling dice values.*/
 function rollDice(){
 	if(turnCount < 4){
 		console.log("rollDice called")
-		for(var i=0; i < 6; i++){
-			// if dice not clicked, set it a random
-			// value between 1-6
+		for(let i=0; i < 6; i++){
+			// If dice not clicked, set it a random value between 1-6
 			if(diceArr[i].clicked === 0){
 				diceArr[i].value = Math.floor((Math.random() * 6) + 1);
 			}
-			//if the dice is clicked, do nothing
+			// If the dice is clicked, do nothing (debug statement)
+			else{
+				console.log(diceArr[i] + " is clicked ")
+			}
 		}
 		updateDiceImg();
-		//increment the number of turns the player has used
-		turnCount += 1;
-		
 	}
-	else{
-		console.log("You've now had 5 turns, game over!")
-	}
+	//else{
+		//console.log("You've had 4 turns, this is your last chance to bank a good score!")
+	//}
 }
 
 /*Updating images of dice given values of rollDice*/
 function updateDiceImg(){
-	var diceImage;
-	for(var i = 0; i < 6; i++){
-		console.log(diceArr[i].id + " has value " + diceArr[i].value)
-		
+	let diceImage;
+	for(let i = 0; i < 6; i++){
+		//console.log(diceArr[i].id + " has value " + diceArr[i].value)
 		diceImage = "images/" + diceArr[i].value + ".png";
 		document.getElementById(diceArr[i].id).setAttribute("src", diceImage);
 	}
 }
 
 function diceClick(img){
-	//first we get the data-number for the dice we clicked on (0-5)
-	var i = img.getAttribute("data-number");
-
-	//toggles the transparent-ish back and forth
-	img.classList.toggle("transparent");
-	
+	// First we get the data-number for the dice we clicked on (0-5)
+	let i = img.getAttribute("data-number");
+	// If dice was unclicked, set it to clicked
 	if(diceArr[i].clicked === 0){
-		diceArr[i].clicked == 1;
-		console.log(diceArr[i].clicked)
+		diceArr[i].clicked = 1;
+		//diceArr[i].counted = true;
+		img.style.opacity = 0.5;
+		//console.log(i + " counted state = " + diceArr[i].counted)
 	}
-	else{
-		diceArr[i].clicked = 0;
-	}
-
 }
 
-/* Calculate score */
+/* TODO: Checks if the roll was a Farkle.
+function checkFarkle(){
+
+}*/
+
+/* Sets all dice back to unclicked. */
+function setAllUnclicked(){
+	for(let i = 0; i < 6; i++){
+		diceArr[i].clicked = 0;
+	}
+}
+
+/* Sets all dice back to untransparent. */
+function setAllUntransparent(){
+	for(let i = 0; i < 6; i++){
+		let diceId = "die" + (i + 1);
+		document.getElementById(diceId).style.opacity = 1;
+	}
+}
+
+/* Calculate score. */
 function calculateTurnScore() {
-	var ones = [];
-	var twos = [];
-	var threes = [];
-	var fours = [];
-	var fives = [];
-	var sixes = [];
+	let ones = [];
+	let twos = [];
+	let threes = [];
+	let fours = [];
+	let fives = [];
+	let sixes = [];
 
-	var onesScore = 0;
-	var twosScore = 0;
-	var threesScore = 0;
-	var foursScore = 0;
-	var fivesScore = 0;
-	var sixesScore = 0;
-
-
-	// array to keep partial scores in
-	var scoresArr = []
-
-	// array to keep each turn's score in
-	var tempScoresArr = [];
-
-	
+	let onesScore = 0;
+	let twosScore = 0;
+	let threesScore = 0;
+	let foursScore = 0;
+	let fivesScore = 0;
+	let sixesScore = 0;
 
 	/* Go through each dice position and if it is clicked, add its value to the right list,
-	TODO: finish implementing the inclusion of the clicked state feature where we take 
-	clicked into account. */
-	for(var i=0; i < 6; i++){
-		if(diceArr[i].clicked === 0){
-			if(diceArr[i].value === 1){
-				ones.push(1)
-			}
-			if(diceArr[i].value === 2){
-				twos.push(2)
-			}
-			if(diceArr[i].value === 3){
-				threes.push(3)
-			}
-			if(diceArr[i].value === 4){
-				fours.push(4)
-			}
-			if(diceArr[i].value === 5){
-				fives.push(5)
-			}
-			if(diceArr[i].value === 6){
-				sixes.push(6)
+	TODO: make it so we do not include die clicked on previous rolls (within same turn)
+	when we calculate the roll score, find a way to indicate when a die has already been counted
+	(add counted property to dice object?). */
+	for(let i=0; i < 6; i++){
+		if (diceArr[i].clicked === 1) {
+			switch (diceArr[i].value){
+				case 1: ones.push(1);
+				break;
+
+				case 2: twos.push(2);
+				break;
+
+				case 3: threes.push(3);
+				break;
+
+				case 4: fours.push(4);
+				break;
+
+				case 5: fives.push(5);
+				break;
+
+				case 6: sixes.push(6);
+				break;
 			}
 		}
 	}
@@ -120,115 +131,185 @@ function calculateTurnScore() {
 	/* Calculate the length of each list and add points as appropriate,
 	my assumption here that if have four 5s, for example, that
 	the points stack up as the (total for three 5s + one 5), etc.
-	Note: If given more time I would code this in a more efficient/elegant way. */
-	if(ones.length >= 1){
-		if(ones.length === 1){
-			onesScore = 100
-		}
-		if(ones.length === 2){
-			onesScore = 200
-		}
-		if(ones.length === 3){
-			onesScore = 1000
-		}
-		if(ones.length === 4){
-			onesScore = 1100
-		}
-		if(ones.length === 5){
-			onesScore = 1200
-		}
-		if(ones.length === 6){
-			onesScore = 2000
-		}
+	Another assumption: if they click four or five 2s (or 3s, 4s, or 6s), they receive
+	the points for three of that number (aka four 2s gets 200 points).
+	Note: Updated to switch-case logic. */
+	
+	switch(ones.length){
+		case 1: onesScore = 100;
+		break;
+		
+		case 2: onesScore = 200;
+		break;
 
-		scoresArr.push(onesScore)
+		case 3: onesScore = 1000;
+		break;
+
+		case 4: onesScore = 1100;
+		break; 
+
+		case 5: onesScore = 1200;
+		break;
+
+		case 6: onesScore = 2000;
+		break;
+	}
+
+	switch(twos.length){
+		case 3: twosScore = 200;
+		break;
+
+		case 4: twosScore = 200;
+		break;
+
+		case 5: twosScore = 200;
+		break;
+
+		case 6: twosScore = 400;
+		break;
+
+		default: twosScore = 0;
+	}
+
+	switch(threes.length){
+		case 3: threesScore = 300;
+		break;
+
+		case 4: threesScore = 300;
+		break;
+
+		case 5: threesScore = 300;
+		break;
+
+		case 6: threesScore = 600;
+		break;
+
+		default: threesScore = 0;
+	}
+
+	switch(fours.length){
+		case 3: foursScore = 400;
+		break;
+
+		case 4: foursScore = 400;
+		break;
+
+		case 5: foursScore = 400;
+		break;
+
+		case 6: foursScore = 800;
+		break;
+
+		default: foursScore = 0;
+	}
+
+	switch(fives.length){
+		case 1: fivesScore = 50;
+		break;
+		
+		case 2: fivesScore = 100;
+		break;
+
+		case 3: fivesScore = 500;
+		break;
+
+		case 4: fivesScore = 550;
+		break; 
+
+		case 5: fivesScore = 600;
+		break;
+
+		case 6: fivesScore = 1000;
+		break;
+	}
+
+	switch(sixes.length){
+		case 3: sixesScore = 600;
+		break;
+
+		case 4: sixesScore = 600;
+		break;
+
+		case 5: sixesScore = 600;
+		break;
+
+		case 6: sixesScore = 1200;
+		break;
+
+		default: sixesScore = 0;
 	}
 	
-	if(twos.length >= 1){
-		if(twos.length === 3){
-			twosScore = 200
-		}
-		if(twos.length === 6){
-			twosScore = 400
-		}
-		scoresArr.push(twosScore)
-	}
-	if(threes.length >= 1){
-		if(threes.length === 3){
-			threesScore = 300
-		}
-		if(threes.length === 6){
-			threesScore = 600
-		}
-		scoresArr.push(threesScore)
-	}
-	if(fours.length >= 1){
-		if(fours.length === 3){
-			foursScore = 400
-		}
-		if(fours.length === 6){
-			foursScore = 800
-		}
-		scoresArr.push(foursScore)
-	}
-	if(fives.length >= 1){
-		if(fives.length === 1){
-			fivesScore = 50
-		}
-		if(fives.length === 2){
-			fivesScore = 100
-		}
-		if(fives.length === 3){
-			fivesScore = 500
-		}
-		if(fives.length === 4){
-			fivesScore = 550
-		}
-		if(fives.length === 5){
-			fivesScore = 600
-		}
-		if(fives.length === 6){
-			fivesScore = 1000
-		}
-
-		scoresArr.push(fivesScore)
-	}
-	if(sixes.length >= 1){
-		if(sixes.length === 3){
-			sixesScore = 600
-		}
-		if(sixes.length === 6){
-			sixesScore = 1200
-		}
-		scoresArr.push(sixesScore)
-	}
-
-	console.log(scoresArr)
-	//add up everything in scoresArr for total turn points
-	var sum = 0
-	for(var i = 0; i < scoresArr.length; i++){
-		sum += scoresArr[i];
-	}
+	// Add up each {numbers}Score for total points for this roll
+	let sum = onesScore + twosScore + threesScore + foursScore + fivesScore + sixesScore;
+	
 	if(sum === 0){
 		console.log("Farkle! Your turn is over.")
 	}
+	console.log("roll sum is " + sum)
 
-	/* TODO: finish implementing adding this current sum to the array of scores, 
-	then sum over the scores to find the new sum to return (and thus be displayed)
-	tempScoresArr.push(sum)*/
+	// Add this roll score to the total turn score array
+	rollScoresForTurnArr.push(sum);
+	console.log("rollScoresForTurnArr is ")
+	console.log(rollScoresForTurnArr)
 
 	return sum
-	
 }
 
-
-
-
 /* Display the player's score and the number of turns they've taken. */
-function displayScore(){
-	const scoreDisplay = document.getElementById('score')
-	let score = calculateTurnScore()
-	scoreDisplay.innerHTML = score
-	const turnDisplay = document.getElementById('turns')
+function displayTempScore(){
+	console.log("displayTempScore called")
+	// Calculate score for that roll
+	let tempScore = calculateTurnScore()
+	// Displaying
+	const scoreDisplay = document.getElementById("row-score")
+	scoreDisplay.innerHTML = tempScore
+	const turnDisplay = document.getElementById('row-turns')
 	turnDisplay.innerHTML = turnCount
+}
+
+/* Increase turn count, display the player's score and the number of turns they've taken,
+set all dice to unclicked/untransparent to prepare for next turn. */
+function bankScore(){
+	console.log("bank Score called")
+	//showTotalScoresArrAndSum();
+	let turnSum = 0;
+	turnCount += 1;
+	
+	/* Currently, take only last term on totalScoresArr 
+	(which will be cumulative over all rolls in turn)*/
+	turnSum = rollScoresForTurnArr[rollScoresForTurnArr.length - 1];
+
+	// Display sum and turn count
+	const scoreDisplay = document.getElementById("row-score")
+	scoreDisplay.innerHTML = turnSum
+	const turnDisplay = document.getElementById('row-turns')
+	turnDisplay.innerHTML = turnCount
+	
+	// Set ready for next turn
+	setAllUnclicked();
+	setAllUntransparent();
+	
+	// Empty the array of roll scores for the turn
+	rollScoresForTurnArr = [];
+
+	// Add turnSum to an array of scores for each turn
+	turnScoresForTotalArr.push(turnSum);
+	console.log("turnScoresForTotalArr is ")
+	console.log(turnScoresForTotalArr)
+
+	// Display last chance message
+	//TODO: change to display on screen not console
+	if(turnCount === 4){
+		console.log("You've had 4 turns, this is your last chance to bank a good score!")
+	}
+
+	// Display ultimate score after 5 turns
+	//TODO: change to display final message and score on screen not console
+	if(turnCount === 5){
+		console.log("Game over! Your final score is: ")
+		for(let i = 0; i < turnScoresForTotalArr.length; i++){
+			ultimateScore += turnScoresForTotalArr[i];
+		}
+		console.log(ultimateScore)
+	}
 }
